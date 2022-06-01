b265dbfeef0e61a1d0d6e35f37c0eee5361a2ab6
Aleksandr Mihailov <mikhaylov.av22@gmail.com>
2022-06-01 11:01:49 +0300

    Add authorization for Log
    фы
    '

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 module Admin
-  class LogsController < ApplicationController
+  class LogsController < BaseController
+    before_action :authorize_user!
+    after_action :verify_authorized
+
     def index
       @pagy, @logs = pagy(Log.includes([:user]).order(created_at: :desc), items: 20)
+    end
+
+    def authorize_user!
+      authorize(@log || Log)
     end
   end
 end
